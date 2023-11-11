a2f8a81717e49d7144f9ce7dc252ab97360b2003
Cody Tipping <Cody Tipping@CODYTIPPING>
2023-11-11 10:50:00 -0500

    Add project files.

---
+++ IdeaBunker/Startup.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using IdeaBunker.Models;
+using IdeaBunker.Data;
+using IdeaBunker.Areas.Identity.Data;
+
+namespace IdeaBunker;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }   
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<IdeaBunkerContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("IdeaBunkerContextConnection")));
+
+        services.AddDefaultIdentity<IdeaBunkerUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<IdeaBunkerContext>();
+
+        services.AddControllersWithViews();
+        services.AddRazorPages();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            //app.UseMigrationsEndPoint();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            endpoints.MapRazorPages();
+        });
+    }
+}
